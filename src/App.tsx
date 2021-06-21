52d95b7d1f9dd82cf10fb3f2cbde5cd7950c4480
Du Fontes <dufontes@gmail.com>
2021-06-21 13:59:42 -0300

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Button } from './components/Button'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Button text="botao" />
+      <Button />
+      <Button text="botao2" />
+      <Button text="botao3" />
+      <h1>Hello World du</h1>
+    </>
   );
 }
 
