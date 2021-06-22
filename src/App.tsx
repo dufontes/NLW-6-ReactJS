80bec1aa6645bdd1ddfd7a68101c0c08a67e18d7
Eduardo Fontes <dufontes@gmail.com>
2021-06-21 23:43:47 -0300

    pages: home, newroom

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
-import { Button } from './components/Button'
+import { BrowserRouter, Route } from 'react-router-dom';
+import { Home } from './pages/Home';
+import { NewRoom } from './pages/NewRoom';
 
 function App() {
   return (
-    <>
-      <Button text="botao" />
-      <Button />
-      <Button text="botao2" />
-      <Button text="botao3" />
-      <h1>Hello World du</h1>
-    </>
+    <BrowserRouter>
+      <Route path="/" exact component={Home} />
+      <Route path="/rooms/new" component={NewRoom} />
+    </BrowserRouter>
   );
 }
 
