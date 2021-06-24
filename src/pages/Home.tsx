4d0b76320b6561981aa724d83e3702a7684a8b98
Eduardo Fontes <dufontes@gmail.com>
2021-06-24 07:20:59 -0300

    aula 2

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,39 +6,47 @@ import googleIconImg from '../assets/images/google-icon.svg';
 
 import { Button } from '../components/Button';
 
+import { useAuth } from '../hooks/useAuth';
+
 import '../styles/auth.scss'
 
 export function Home() {
-    const history = useHistory();
+	const history = useHistory();
+	const { user, signInWithGoogle } = useAuth();
 
-    function navigateToNewRoom() {
-        history.push('/rooms/new');
-    }
+	async function handleCreateRoom() {
+		if (!user) {
+			await signInWithGoogle();
+		}
 
-    return (
-        <div id="page-auth">
-            <aside>
-                <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
-                <strong>Crie salas de Q&amp;A ao-vivo</strong>
-                <p>Tire dúvidas da sua audiência em tempo-real</p>
-            </aside>
-            <main>
-                <div className="main-content">
-                    <img src={logoImg} alt="Letmeask" />
-                    <button onClick={navigateToNewRoom} className="create-room">
-                        <img src={googleIconImg} alt="Logo do Google" />
-                        Crie sua sala com o google
-                    </button>
-                    <div className="separator">ou entre em uma sala</div>
-                    <form>
-                        <input
-                            type="text"
-                            placeholder="Digite o código da sala"
-                        />
-                        <Button type="submit">Entrar na sala</Button>
-                    </form>
-                </div>
-            </main>
-        </div>
-    )
+		history.push('/rooms/new');
+
+	}
+
+	return (
+		<div id="page-auth">
+			<aside>
+				<img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
+				<strong>Crie salas de Q&amp;A ao-vivo</strong>
+				<p>Tire dúvidas da sua audiência em tempo-real</p>
+			</aside>
+			<main>
+				<div className="main-content">
+					<img src={logoImg} alt="LetMeAsk" />
+					<button onClick={handleCreateRoom} className="create-room">
+						<img src={googleIconImg} alt="Logo do Google" />
+						Crie sua sala com o google
+					</button>
+					<div className="separator">ou entre em uma sala</div>
+					<form>
+						<input
+							type="text"
+							placeholder="Digite o código da sala"
+						/>
+						<Button type="submit">Entrar na sala</Button>
+					</form>
+				</div>
+			</main>
+		</div>
+	)
 }
--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -7,29 +7,32 @@ import googleIconImg from '../assets/images/google-icon.svg';
 import { Button } from '../components/Button';
 
 import '../styles/auth.scss'
+// import { useAuth } from '../hooks/useAuth';
 
 export function NewRoom() {
-    return (
-        <div id="page-auth">
-            <aside>
-                <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
-                <strong>Crie salas de Q&amp;A ao-vivo</strong>
-                <p>Tire dúvidas da sua audiência em tempo-real</p>
-            </aside>
-            <main>
-                <div className="main-content">
-                    <img src={logoImg} alt="Letmeask" />
-                    <h2>Criar uma nova sala</h2>
-                    <form>
-                        <input
-                            type="text"
-                            placeholder="Nome da sala"
-                        />
-                        <Button type="submit">Criar sala</Button>
-                    </form>
-                    <p>Quer entrar em uma sala existente? <Link to="/">Clique aqui</Link></p>
-                </div>
-            </main>
-        </div>
-    )
+	// const { user } = useAuth();
+
+	return (
+		<div id="page-auth">
+			<aside>
+				<img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
+				<strong>Crie salas de Q&amp;A ao-vivo</strong>
+				<p>Tire dúvidas da sua audiência em tempo-real</p>
+			</aside>
+			<main>
+				<div className="main-content">
+					<img src={logoImg} alt="Letmeask" />
+					<h2>Criar uma nova sala</h2>
+					<form>
+						<input
+							type="text"
+							placeholder="Nome da sala"
+						/>
+						<Button type="submit">Criar sala</Button>
+					</form>
+					<p>Quer entrar em uma sala existente? <Link to="/">Clique aqui</Link></p>
+				</div>
+			</main>
+		</div>
+	)
 }
